f10ba30e54de2c8fe65adad393240699648996de
Shi-chang <shichangye@outlook.com>
2022-09-12 21:43:08 -0700

    Add IRoom

--- src/model/IRoom.java
+++ src/model/IRoom.java
@@ -1,2 +1,35 @@
-package model;public interface IRoom {
+package model;
+
+/**
+ * An interface for rooms.
+ */
+public interface IRoom {
+
+    /**
+     * Gets the room number.
+     *
+     * @return the room number
+     */
+    public String getRoomNumber();
+
+    /**
+     * Gets the room price.
+     *
+     * @return the room price
+     */
+    public Double getRoomPrice();
+
+    /**
+     * Gets the room type.
+     *
+     * @return the room type
+     */
+    public RoomType getRoomType();
+
+    /**
+     * Returns if the room is free.
+     *
+     * @return true if the room is free, false otherwise
+     */
+    public boolean isFree();
 }
