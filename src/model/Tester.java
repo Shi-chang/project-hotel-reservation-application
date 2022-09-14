6f8005e3f9558e5ebade3cb5c2a7145d0fccf21a
Shi-chang <shichangye@outlook.com>
2022-09-13 22:49:47 -0700

    Change types of dates from String to Date

--- src/model/Tester.java
+++ src/model/Tester.java
@@ -5,18 +5,29 @@ import java.util.Date;
 public class Tester {
 
     public static void main(String[] args) {
+        // Tests the Room class
         Room room = new Room("000", 130.00, RoomType.SINGLE);
         System.out.println(room);
 
+        // Tests the FreeRoom class
         FreeRoom freeRoom = new FreeRoom("001", RoomType.DOUBLE);
         System.out.println(freeRoom);
 
-        Customer customer = new Customer("Alan", "Turing", "alanturing@gmail.com");
-        System.out.println(customer);
+        // Tests the Customer class
+        Customer customer1 = new Customer("Alan", "Turing", "alanturing@gmail.com");
+        System.out.println(customer1);
 
-        String checkInDate = "2022/12/12";
-        String checkOutDate = "2022/12/15";
-        Reservation reservation = new Reservation(customer, room, checkInDate, checkOutDate);
+        // Tests the Customer class when the email address is invalid
+        try {
+            Customer customer2 = new Customer("Alan", "Turing", "alanturinggmail.com");
+        } catch (IllegalArgumentException e) {
+            System.out.println("This is expected.");
+        }
+
+        // Tests the Reservation class
+        Date checkInDate = new Date();
+        Date checkOutDate = new Date();
+        Reservation reservation = new Reservation(customer1, room, checkInDate, checkOutDate);
         System.out.println(reservation);
     }
 }
